c567ea35596a89c3a1844c20adaafac0fa351e26
Teerth <teerthsankesara@gmail.com>
2022-01-23 00:20:48 +0530

    header half done @55:08

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import Head from 'next/head';
+import Header from '../components/Header';
 const Home = () => {
 	return (
 		<div>
-			<h1>Hello World</h1>
+			<Head>
+				<title>Hulu</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<Header />
 		</div>
 	);
 };

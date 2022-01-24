551ae9602acf2efaf6c82efc9944aaf86af8caf7
Teerth <teerthsankesara@gmail.com>
2022-01-24 16:05:26 +0530

    Built Dont, Now ReadMe Writing and Deployment

--- components/Results.js
+++ components/Results.js
@@ -1,5 +1,13 @@
-const Results = () => {
-	return <div></div>;
+import Thumbnail from './Thumbnail';
+import FlipMove from 'react-flip-move';
+const Results = ({ results }) => {
+	return (
+		<FlipMove className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-4 3xl:flex flex-wrap justify-content'>
+			{results.map((result) => {
+				return <Thumbnail key={result.id} result={result} />;
+			})}
+		</FlipMove>
+	);
 };
 
 export default Results;
